a1ff9016207104dd27481bd4315605a4c571b07e
Motorhead1991 <motor-head-1991@hotmail.com>
2013-08-31 16:58:38 -0700

    CWM buildable. Initial commit

---
+++ Android.mk
@@ -0,0 +1,3 @@
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),amazing_cdma)
+include $(call first-makefiles-under,$(call my-dir))
+endif
---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/full_amazing_cdma.mk 
